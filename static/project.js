5a0bca805e7eda19a5660283a8ca68ddb66b6474
IzzyFG <izardfg@gmail.com>
2024-04-15 20:51:37 -0400

    setting up branch

---
+++ static/project.js
@@ -0,0 +1,19 @@
+// Lesson JS
+
+//Flashcards
+// Loop through flashcards 
+// {% for flashcard in lesson.flashcards %}
+//   <p>{{ flashcard.title }}</p>
+//   <img src="{{ flashcard.image_url }}" alt="Flashcard Image">
+//   <p>{{ flashcard.text }}</p>
+// {% endfor %}
+
+function makeCard (flashcard){
+	//card
+
+	//card-front
+
+	//card-back
+
+
+};
