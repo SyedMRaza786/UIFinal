389b588ec908e5113ffaeafa40f3a42851559099
IzzyFG <izardfg@gmail.com>
2024-04-17 06:27:10 -0400

    audio bank

--- static/learn.js
+++ static/learn.js
@@ -44,6 +44,65 @@ function checkCards(){
 	}
 }
 
+function makeSingleAudio(i, text, aux_clip){
+	var audio_col = 
+		`<div class="droppable ui-widget-header ui-state-default padded" data=${i} id ="drop_{i}">
+			<div class='row' id = 'desc_row_${i}'>
+				<div id = 'aud_description_${i}'>
+					${text}
+				</div>
+			</div>
+
+			<br>
+
+			<div class='row' id = 'audio_row_${i}'>
+				<div id = 'audio_${i}'>
+					<audio controls>
+						<source src='${aux_clip}' type="audio/mpeg">
+						Your browser does not support the audio element.
+					</audio>
+				</div>
+			</div>
+		</div>`;
+	return audio_col;
+}
+
+function makeAudioAnswer(i, answer){
+	var html =
+		$("<li class='ui-widget-content ui-corner-tr listI' data="+i+">"+
+		"<div class='row align-items-start' "+
+		"id ="+i+">" +
+			"<div class='col'> <br>"+ answer + "<br> </div>" +
+		"</div>"+
+		'</li>');
+	return html;	
+}
+
+function makeAudioBlock(){
+	for (let i = 0; i<lesson.audio_clips.length; i++){
+		var temp_audio = makeSingleAudio(i, lesson.audio_clips[i]["text"],lesson.audio_clips[i]["sound_url"]);
+
+		var temp_answer = makeAudioAnswer(i, lesson.audio_clips[i]["answer"])
+		
+		$(temp_audio).appendTo("#audio-col");
+		$(temp_answer).appendTo("#word-bank");
+		
+		temp_answer.draggable( {revert: "invalid"});
+	}
+	
+}
+function resetAudio(){
+	window.location.reload();
+};
+
+
+function checkAudio(){
+	if( lesson.hasOwnProperty("audio_clips")){
+		makeAudioBlock();
+	}
+}
+
+
 $.fn.multiline = function(text){
     this.text(text);
     this.html(this.html().split("\\n").join("<br/>"));
@@ -63,13 +122,30 @@ function updatePage(){
 	updateTitle(lesson["title"]);
 	updateBody(lesson["body"]);
 	checkCards();
+	checkAudio();
 }
 
 
 //Functions called on start
 $(document).ready(function(){
 	updatePage();
-	// console.log("lesson: ");
-	// console.log(lesson);
 
+	$( ".droppable" ).droppable({
+		classes: {
+			"ui-droppable-hover": "lightYellow"
+		},
+		drop: function(event, ui) { 
+			$(ui.draggable).draggable('disable');
+
+			if ($(ui.draggable).attr("data") ==$(this).attr("data") ){
+				$(this).addClass('green');
+				$(ui.draggable).addClass('green');     
+			}
+			else{
+				$(this).addClass('red');
+				$(ui.draggable).addClass('red');   
+
+			}
+		  }
+	});
 });
